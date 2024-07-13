1a503811c0b9ba04fbaf6ab569fdf1f2482340e3
guiromao87 <guilhermersimoes87@gmail.com>
2024-07-13 11:42:53 -0300

    novo codigo

--- src/main/java/com/example/integration/Calculadora.java
+++ src/main/java/com/example/integration/Calculadora.java
@@ -3,6 +3,7 @@ package com.example.integration;
 public class Calculadora {
 
     public double soma(double num1, double num2) {
+        System.out.println("soma...");
         return num1 + num2;
     }
 }
